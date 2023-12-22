d1a24db0ae98137c3114ac2c21c254db3c940a14
alegitmad <alessandro.maddalena@students.uniroma2.eu>
2023-12-22 16:57:23 +0100

    homepage and login pages

--- src/main/java/com/example/lessonlink/view1/BookLessonControllerG.java
+++ src/main/java/com/example/lessonlink/view1/BookLessonControllerG.java
@@ -6,14 +6,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class BookLessonControllerG extends Application {
         @Override
         public void start(Stage stage) throws IOException {
             FXMLLoader fxmlLoader = new FXMLLoader(BookLessonControllerG.class.getResource("home.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 800, 450);
-            //scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("css/styles.css")).toExternalForm());
+            Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
             stage.setTitle("LessonLink");
             stage.setScene(scene);
             stage.show();
